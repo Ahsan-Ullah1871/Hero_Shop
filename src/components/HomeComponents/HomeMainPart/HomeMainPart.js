b3d72380a994726e635a147c6d06071161eed3fe
mow-sumi <mow521202sumi@gmail.com>
2021-05-31 10:25:31 +0600

    hero shop food project

--- src/components/HomeComponents/HomeMainPart/HomeMainPart.js
+++ src/components/HomeComponents/HomeMainPart/HomeMainPart.js
@@ -2,12 +2,15 @@ import React from "react";
 import styles from "./HomeMainPart.module.css";
 import Header from "../Header/Header";
 import ProductsMainPart from "../ProductsComponent/ProductsMainPart/ProductsMainPart";
+import Feature from "../Feature/Feature";
 
 const HomeMainPart = () => {
 	return (
 		<div className={styles.HomeMainPart}>
 			<Header />
+			<Feature />
 			<ProductsMainPart />
+			
 		</div>
 	);
 };
